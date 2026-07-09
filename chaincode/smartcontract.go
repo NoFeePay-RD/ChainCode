70aea092c51e58f7de1dc63e12b753c084838e6a
Sanuja Methmal <it21258176@my.sliit.lk>
2026-07-09 15:58:00 +0000

    feature: handle open customer fund deposits
    This is a initial stage of interacting with customers funds. This can retriew all the available customer deposits. And depositing customer fund should be done one at a time. This is because if one ledger update transaction failed, contract will reject the entire bulk.
    
    In this stage, identiy verification is not considered. Meaning anyone can add funds to the ledger. In future, only our cash manager service should be able to do so.
    
    Use 'peer chaincode query' and then 'peer chaincode invoke' command to test this behaviour with appropriate argument values.

--- chaincode/smartcontract.go
+++ chaincode/smartcontract.go
@@ -12,183 +12,80 @@ type SmartContract struct {
 	contractapi.Contract
 }
 
-// Asset describes basic details of what makes up a simple asset
-// Insert struct field in alphabetic order => to achieve determinism across languages
-// golang keeps the order when marshal to json but doesn't order automatically
-type Asset struct {
-	AppraisedValue int    `json:"AppraisedValue"`
-	Color          string `json:"Color"`
-	ID             string `json:"ID"`
-	Owner          string `json:"Owner"`
-	Size           int    `json:"Size"`
+type FundDepositEntry struct {
+	Amount   float64 `json:"amount"`
+	Currency string  `json:"currency"`
 }
 
-// InitLedger adds a base set of assets to the ledger
-func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	assets := []Asset{
-		{ID: "asset1", Color: "blue", Size: 5, Owner: "Tomoko", AppraisedValue: 300},
-		{ID: "asset2", Color: "red", Size: 5, Owner: "Brad", AppraisedValue: 400},
-		{ID: "asset3", Color: "green", Size: 10, Owner: "Jin Soo", AppraisedValue: 500},
-		{ID: "asset4", Color: "yellow", Size: 10, Owner: "Max", AppraisedValue: 600},
-		{ID: "asset5", Color: "black", Size: 15, Owner: "Adriana", AppraisedValue: 700},
-		{ID: "asset6", Color: "white", Size: 15, Owner: "Michel", AppraisedValue: 800},
-	}
-
-	for _, asset := range assets {
-		assetJSON, err := json.Marshal(asset)
-		if err != nil {
-			return err
-		}
-
-		err = ctx.GetStub().PutState(asset.ID, assetJSON)
-		if err != nil {
-			return fmt.Errorf("failed to put to world state. %v", err)
-		}
-	}
-
-	return nil
+type FundDepositResponse = struct {
+	Amount          float64 `json:"amount"`
+	CustomerAddress string  `json:"customerAddress"`
+	TransactionID   string  `json:"transactionID"`
+	Currency        string  `json:"currency"`
 }
 
-// CreateAsset issues a new asset to the world state with given details.
-func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, color string, size int, owner string, appraisedValue int) error {
-	exists, err := s.AssetExists(ctx, id)
-	if err != nil {
-		return err
-	}
-	if exists {
-		return fmt.Errorf("the asset %s already exists", id)
+func (s *SmartContract) DepositFunds(ctx contractapi.TransactionContextInterface, customerAddress string, amount float64, currency string) error {
+	if amount <= 0 {
+		return fmt.Errorf("deposit amount must be greater than zero")
 	}
 
-	asset := Asset{
-		ID:             id,
-		Color:          color,
-		Size:           size,
-		Owner:          owner,
-		AppraisedValue: appraisedValue,
+	txID := ctx.GetStub().GetTxID()
+
+	fundDeposit := FundDepositEntry{
+		Amount:   amount,
+		Currency: currency,
 	}
-	assetJSON, err := json.Marshal(asset)
+
+	fundDepositJSON, err := json.Marshal(fundDeposit)
 	if err != nil {
 		return err
 	}
 
-	return ctx.GetStub().PutState(id, assetJSON)
+	// Create a unique composite key: F~CustomerAddress~TransactionID
+	compositeKey, err := ctx.GetStub().CreateCompositeKey("F", []string{customerAddress, txID})
+	if err != nil {
+		return fmt.Errorf("failed to create composite key: %v", err)
+	}
+
+	return ctx.GetStub().PutState(compositeKey, fundDepositJSON)
 }
 
-// ReadAsset returns the asset stored in the world state with given id.
-func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, id string) (*Asset, error) {
-	assetJSON, err := ctx.GetStub().GetState(id)
+// GetAllCustomerFunds retrieves all customer funds from the ledger
+func (s *SmartContract) GetAllCustomerFunds(ctx contractapi.TransactionContextInterface) ([]FundDepositResponse, error) {
+	iterator, err := ctx.GetStub().GetStateByPartialCompositeKey("F", []string{})
 	if err != nil {
-		return nil, fmt.Errorf("failed to read from world state: %v", err)
+		return nil, fmt.Errorf("failed to read customer funds: %v", err)
 	}
-	if assetJSON == nil {
-		return nil, fmt.Errorf("the asset %s does not exist", id)
-	}
+	defer iterator.Close()
 
-	var asset Asset
-	err = json.Unmarshal(assetJSON, &asset)
-	if err != nil {
-		return nil, err
-	}
+	var results []FundDepositResponse
 
-	return &asset, nil
-}
-
-// UpdateAsset updates an existing asset in the world state with provided parameters.
-func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface, id string, color string, size int, owner string, appraisedValue int) error {
-	exists, err := s.AssetExists(ctx, id)
-	if err != nil {
-		return err
-	}
-	if !exists {
-		return fmt.Errorf("the asset %s does not exist", id)
-	}
-
-	// overwriting original asset with new asset
-	asset := Asset{
-		ID:             id,
-		Color:          color,
-		Size:           size,
-		Owner:          owner,
-		AppraisedValue: appraisedValue,
-	}
-	assetJSON, err := json.Marshal(asset)
-	if err != nil {
-		return err
-	}
-
-	return ctx.GetStub().PutState(id, assetJSON)
-}
-
-// DeleteAsset deletes an given asset from the world state.
-func (s *SmartContract) DeleteAsset(ctx contractapi.TransactionContextInterface, id string) error {
-	exists, err := s.AssetExists(ctx, id)
-	if err != nil {
-		return err
-	}
-	if !exists {
-		return fmt.Errorf("the asset %s does not exist", id)
-	}
-
-	return ctx.GetStub().DelState(id)
-}
-
-// AssetExists returns true when asset with given ID exists in world state
-func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
-	assetJSON, err := ctx.GetStub().GetState(id)
-	if err != nil {
-		return false, fmt.Errorf("failed to read from world state: %v", err)
-	}
-
-	return assetJSON != nil, nil
-}
-
-// TransferAsset updates the owner field of asset with given id in world state, and returns the old owner.
-func (s *SmartContract) TransferAsset(ctx contractapi.TransactionContextInterface, id string, newOwner string) (string, error) {
-	asset, err := s.ReadAsset(ctx, id)
-	if err != nil {
-		return "", err
-	}
-
-	oldOwner := asset.Owner
-	asset.Owner = newOwner
-
-	assetJSON, err := json.Marshal(asset)
-	if err != nil {
-		return "", err
-	}
-
-	err = ctx.GetStub().PutState(id, assetJSON)
-	if err != nil {
-		return "", err
-	}
-
-	return oldOwner, nil
-}
-
-// GetAllAssets returns all assets found in world state
-func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]*Asset, error) {
-	// range query with empty string for startKey and endKey does an
-	// open-ended query of all assets in the chaincode namespace.
-	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
-	if err != nil {
-		return nil, err
-	}
-	defer resultsIterator.Close()
-
-	var assets []*Asset
-	for resultsIterator.HasNext() {
-		queryResponse, err := resultsIterator.Next()
+	for iterator.HasNext() {
+		response, err := iterator.Next()
 		if err != nil {
 			return nil, err
 		}
 
-		var asset Asset
-		err = json.Unmarshal(queryResponse.Value, &asset)
+		var fund FundDepositEntry
+
+		err = json.Unmarshal(response.Value, &fund)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to decode customer fund record: %v", err)
 		}
-		assets = append(assets, &asset)
+
+		// Decode composite key
+		_, attributes, err := ctx.GetStub().SplitCompositeKey(response.Key)
+		if err != nil {
+			return nil, fmt.Errorf("failed to split composite key: %v", err)
+		}
+
+		var FundDepositResponse FundDepositResponse
+		FundDepositResponse.Amount = fund.Amount
+		FundDepositResponse.Currency = fund.Currency
+		FundDepositResponse.CustomerAddress = attributes[0]
+		FundDepositResponse.TransactionID = attributes[1]
+		results = append(results, FundDepositResponse)
 	}
 
-	return assets, nil
+	return results, nil
 }
