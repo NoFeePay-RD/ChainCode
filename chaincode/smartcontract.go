1008cd16d6f1519b67cca55ececdd11ecc77fd01
Sanuja Methmal <it21258176@my.sliit.lk>
2026-07-11 13:03:22 +0000

    feature: handle open customer fund transfer
    Customers can transfer funds to a given merchant. Contract check the customer's balance from the requested currency. If there is enough balance, new transaction record is created in ledger. Those created trunsactions can be queried from a seperate function.
    
    In this stage, identiy verification is not considered. Meaning anyone can transfer funds behalf of the customer. In future, both customer and merchant signatures should be veryfied.
    
    Use 'peer chaincode query' and then 'peer chaincode invoke' command to test this behaviour with appropriate argument values.

--- chaincode/smartcontract.go
+++ chaincode/smartcontract.go
@@ -17,11 +17,11 @@ type FundDepositEntry struct {
 	Currency string  `json:"currency"`
 }
 
-type FundDepositResponse = struct {
+type FundDepositResponse struct {
 	Amount          float64 `json:"amount"`
 	CustomerAddress string  `json:"customerAddress"`
-	TransactionID   string  `json:"transactionID"`
 	Currency        string  `json:"currency"`
+	TransactionID   string  `json:"transactionID"`
 }
 
 func (s *SmartContract) DepositFunds(ctx contractapi.TransactionContextInterface, customerAddress string, amount float64, currency string) error {
@@ -79,12 +79,113 @@ func (s *SmartContract) GetAllCustomerFunds(ctx contractapi.TransactionContextIn
 			return nil, fmt.Errorf("failed to split composite key: %v", err)
 		}
 
-		var FundDepositResponse FundDepositResponse
-		FundDepositResponse.Amount = fund.Amount
-		FundDepositResponse.Currency = fund.Currency
-		FundDepositResponse.CustomerAddress = attributes[0]
-		FundDepositResponse.TransactionID = attributes[1]
-		results = append(results, FundDepositResponse)
+		var fundDepositResponse FundDepositResponse
+		fundDepositResponse.Amount = fund.Amount
+		fundDepositResponse.Currency = fund.Currency
+		fundDepositResponse.CustomerAddress = attributes[0]
+		fundDepositResponse.TransactionID = attributes[1]
+		results = append(results, fundDepositResponse)
+	}
+
+	return results, nil
+}
+
+type FundTransferEntry struct {
+	Amount          float64 `json:"amount"`
+	Currency        string  `json:"currency"`
+	CustomerAddress string  `json:"customerAddress"`
+	MerchantAddress string  `json:"merchantAddress"`
+}
+
+type FundTransferResponse struct {
+	Amount          float64 `json:"amount"`
+	Currency        string  `json:"currency"`
+	CustomerAddress string  `json:"customerAddress"`
+	MerchantAddress string  `json:"merchantAddress"`
+	TransactionID   string  `json:"transactionID"`
+}
+
+func (s *SmartContract) TransferFunds(ctx contractapi.TransactionContextInterface, customerAddress string, amount float64, currency string, merchantAddress string) error {
+	if amount <= 0 {
+		return fmt.Errorf("transfer amount must be greater than zero")
+	}
+
+	//check if the customer has enough funds to transfer
+	// Get all customer funds
+	customerFunds, err := s.GetAllCustomerFunds(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to retrieve customer funds: %v", err)
+	}
+
+	var totalFunds float64
+	for _, fund := range customerFunds {
+		if fund.CustomerAddress == customerAddress && fund.Currency == currency {
+			totalFunds += fund.Amount
+		}
+	}
+
+	if totalFunds < amount {
+		return fmt.Errorf("insufficient funds for transfer: available %.2f, required %.2f", totalFunds, amount)
+	}
+
+	txID := ctx.GetStub().GetTxID()
+
+	fundTransfer := FundTransferEntry{
+		Amount:          amount,
+		Currency:        currency,
+		CustomerAddress: customerAddress,
+		MerchantAddress: merchantAddress,
+	}
+
+	fundTransferJSON, err := json.Marshal(fundTransfer)
+	if err != nil {
+		return err
+	}
+
+	// Create a unique composite key: T~CustomerAddress~TransactionID
+	compositeKey, err := ctx.GetStub().CreateCompositeKey("T", []string{customerAddress, txID})
+	if err != nil {
+		return fmt.Errorf("failed to create composite key: %v", err)
+	}
+
+	return ctx.GetStub().PutState(compositeKey, fundTransferJSON)
+}
+
+func (s *SmartContract) GetAllCustomerTransfers(ctx contractapi.TransactionContextInterface) ([]FundTransferResponse, error) {
+	iterator, err := ctx.GetStub().GetStateByPartialCompositeKey("T", []string{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to read customer transfers: %v", err)
+	}
+	defer iterator.Close()
+
+	var results []FundTransferResponse
+
+	for iterator.HasNext() {
+		response, err := iterator.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		var transfer FundTransferEntry
+
+		err = json.Unmarshal(response.Value, &transfer)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode customer transfer record: %v", err)
+		}
+
+		// Decode composite key
+		_, attributes, err := ctx.GetStub().SplitCompositeKey(response.Key)
+		if err != nil {
+			return nil, fmt.Errorf("failed to split composite key: %v", err)
+		}
+
+		var fundTransferResponse FundTransferResponse
+		fundTransferResponse.Amount = transfer.Amount
+		fundTransferResponse.Currency = transfer.Currency
+		fundTransferResponse.CustomerAddress = attributes[0]
+		fundTransferResponse.MerchantAddress = transfer.MerchantAddress
+		fundTransferResponse.TransactionID = attributes[1]
+		results = append(results, fundTransferResponse)
 	}
 
 	return results, nil
